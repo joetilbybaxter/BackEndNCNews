a15d95728b301a3c52d9b93101289d869f314703
Anthony Medina <antmedina91@gmail.com>
2019-05-11 12:47:39 +0100

    modify varchar length for article body column

--- db/migrations/20190511114557_articles-table.js
+++ db/migrations/20190511114557_articles-table.js
@@ -2,7 +2,7 @@ exports.up = knex => {
   return knex.schema.createTable('articles', articlesTable => {
     articlesTable.increments('article_id').primary();
     articlesTable.string('title').notNullable();
-    articlesTable.string('body').notNullable();
+    articlesTable.string('body', 25000).notNullable();
     articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
     articlesTable
       .string('topic')
