31862cb7c427db769a7b83f29df8dda12de35eb8
Anthony Medina <antmedina91@gmail.com>
2019-05-11 14:32:47 +0100

    seed comments

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,13 +1,22 @@
-const { topicData, userData, articleData } = require('../data');
-const { convertTimestampToDate } = require('../../utils');
+const { topicData, userData, articleData, commentData } = require('../data');
+const {
+  convertTimestampToDate,
+  createRef,
+  formatComments,
+} = require('../../utils');
 
 exports.seed = async knex => {
   await knex.migrate.rollback();
   await knex.migrate.latest();
+
   const topicsPromise = knex('topics').insert(topicData, '*');
   const usersPromise = knex('users').insert(userData, '*');
   await Promise.all([topicsPromise, usersPromise]);
+
   const formattedArticleData = articleData.map(convertTimestampToDate);
   const articleRows = await knex('articles').insert(formattedArticleData, '*');
-  return articleRows;
+
+  const articleIdLookup = createRef(articleRows, 'title', 'article_id');
+  const formattedCommentData = formatComments(commentData, articleIdLookup);
+  return knex('comments').insert(formattedCommentData);
 };
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,21 @@
-exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
+const convertTimestampToDate = ({ created_at, ...otherProperties }) => {
+  if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
 };
+
+const createRef = (arr, key, value) => {
+  return arr.reduce((ref, element) => {
+    ref[element[key]] = element[value];
+    return ref;
+  }, {});
+};
+
+const formatComments = (comments, idLookup) => {
+  return comments.map(({ created_by, belongs_to, ...restOfComment }) => {
+    belongs_to = idLookup[belongs_to];
+    restOfComment = convertTimestampToDate(restOfComment);
+    return { belongs_to, author: created_by, ...restOfComment };
+  });
+};
+
+module.exports = { convertTimestampToDate, createRef, formatComments };
