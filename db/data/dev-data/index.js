b6ff1a9b205a6cc0acdc523b2ac534fe5091e5bb
Anthony Medina <antmedina91@gmail.com>
2019-05-11 11:11:41 +0100

    add raw data for test and development

--- db/data/dev-data/index.js
+++ db/data/dev-data/index.js
@@ -1,1 +1,4 @@
-// require in and export out all dev data
+exports.topicData = require('./topics');
+exports.userData = require('./users');
+exports.articleData = require('./articles');
+exports.commentData = require('./comments');
