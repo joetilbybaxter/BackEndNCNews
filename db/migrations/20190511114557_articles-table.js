5bf2c87852db503609ef0771edab5e5e57191790
Anthony Medina <antmedina91@gmail.com>
2019-05-11 11:55:11 +0100

    create articles table

---
+++ db/migrations/20190511114557_articles-table.js
@@ -0,0 +1,22 @@
+exports.up = knex => {
+  return knex.schema.createTable('articles', articlesTable => {
+    articlesTable.increments('article_id').primary();
+    articlesTable.string('title').notNullable();
+    articlesTable.string('body').notNullable();
+    articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
+    articlesTable
+      .string('topic')
+      .references('slug')
+      .inTable('topics')
+      .notNullable();
+    articlesTable
+      .string('author')
+      .references('username')
+      .inTable('users')
+      .notNullable();
+  });
+};
+
+exports.down = knex => {
+  return knex.schema.dropTableIfExists('articles');
+};
