e6930fce57c7374c4b7f03290dd87dfb5d524343
Anthony Medina <antmedina91@gmail.com>
2019-05-13 01:35:12 +0100

    prepare connection file for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,6 @@
-const dbConfig = require('../knexfile');
+const dbConfig =
+  process.env.NODE_ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
 
 module.exports = require('knex')(dbConfig);
