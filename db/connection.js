6465d807b638a0728579f30d4e90f65eb28d4f2e
JakeTB <jtilbybaxter@gmail.comgit config --global user.name Jakegit config --global user.email jtilbybaxter@gmail.com>
2021-04-09 14:55:47 +0100

    Changes to dbConncections

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const dbConfig =
   process.env.NODE_ENV === 'production'
-    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    ? { client: "pg",
+        connection: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+} }
     : require('../knexfile');
 
 module.exports = require('knex')(dbConfig);
